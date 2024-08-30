388888b7b49634fa5c40983cac7a5af1018cda65
Salem4tech <137092623+Salem4tech@users.noreply.github.com>
2024-08-30 14:33:06 +0100

    update

--- src/components/Home/Unveiling.jsx
+++ src/components/Home/Unveiling.jsx
@@ -5,7 +5,7 @@ const Unveiling = () => {
   return (
     <section>
       <div className=" bg-dark">
-        <div className="max-w-[85%] mx-auto px-1 pt-12 xl:pt-20 flex flex-col-reverse items-center xl:grid xl:grid-cols-2 h-full max-w gap-x-10">
+        <div className="max-w-[85%] mx-auto px-1 pt-16 xl:pt-20 flex flex-col-reverse items-center xl:grid xl:grid-cols-2 h-full max-w gap-x-10 mb-12">
           <div className=" xl:m-4 md:grid md:basis-1/3">
             <img
               src={UnveilingImg}
@@ -17,14 +17,14 @@ const Unveiling = () => {
             <h6 className=" font-semibold text-lg xl:text-2xl  text-[#5846FB] mb-4">
               What is techies Community
             </h6>
-            <h1 className="text-4xl xl:leading-[120%] xl:text-7xl font-semibold mb-2 xl:mr-20 text-white">
+            <h1 className="text-4xl xl:leading-[120%] xl:text-7xl font-normal mb-4 xl:mr-20 text-white">
             Unveiling TechiesAfrica Community
             </h1>
-            <p className=" mb-4 xl:mb-10 mr-0 xl:mr-20 text-base xl:text-lg font-medium text-white">
+            <p className=" mb-4 xl:mb-10 mr-0 xl:mr-22 text-xs xl:text-lg font-light text-[#F2F2F2]">
               Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet
               faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.
             </p>
-            <div className=" w-60">
+            <div className=" w-60 mb-16 xl-mb-0">
               <Button>Join our community</Button>
             </div>
           </div>
