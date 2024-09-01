36be6a07bb39669987487573cc3f36fad3719ec1
Salem4tech <137092623+Salem4tech@users.noreply.github.com>
2024-09-01 21:04:48 +0100

    Updating..

--- src/components/Home/Idea.jsx
+++ src/components/Home/Idea.jsx
@@ -5,7 +5,7 @@ const Idea = () => {
   return (
     <section>
       <div className="bg-white relative pb-12 h-full">
-        <div className="max-w-[85%] mx-auto px-1 pt-20  items-center xl:grid lg:grid-cols-2 h-full max-w">
+        <div className="max-w-[85%] mx-auto px-1 py-20 items-center xl:grid lg:grid-cols-2 h-full max-w">
           <div className="flex flex-col justify-center h-full basis-1/3 text-dark">
             <h6 className=" font-semibold text-2xl text-[#5846FB] mb-8">
               Who we are at Techies Africa
