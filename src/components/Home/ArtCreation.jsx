1a9024389f6b1a76dc16b126ec1897e48a25d7ef
Salem4tech <137092623+Salem4tech@users.noreply.github.com>
2024-09-02 12:30:24 +0100

    Updating

--- src/components/Home/ArtCreation.jsx
+++ src/components/Home/ArtCreation.jsx
@@ -10,12 +10,12 @@ const ArtCreation = () => {
           <div className=" xl:m-4 md:grid md:basis-1/3">
             <img src={Video} alt="" className="w-auto h-full object-cover" />
           </div>
-          <div className="flex flex-col xl:pl-44 justify-center h-full md:grid md:basis-2/3 text-dark">
+          <div className="flex flex-col xl:pl-20 justify-center h-full md:grid md:basis-2/3 text-dark">
             <h6 className=" font-semibold xl:text-2xl  text-[#5846FB] mb-4">
-              What is techies Community
+              Our Work Process
             </h6>
             <h1 className="text-4xl xl:leading-[120%] xl:text-7xl font-normal mb-4 xl:mr-20 text-dark">
-              Unveiling TechiesAfrica Community
+              The Art of Our Creative Process
             </h1>
             <p className=" mb-4 xl:mb-10 mr-0 xl:mr-22 xl:text-lg font-light text-dark">
               Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet
