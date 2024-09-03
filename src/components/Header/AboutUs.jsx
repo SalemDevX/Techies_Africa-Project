b6589bd9572d0d083c7414c2b4234adeb9d65fdb
Salem4tech <137092623+Salem4tech@users.noreply.github.com>
2024-09-03 21:31:52 +0100

    Update

--- src/components/Header/AboutUs.jsx
+++ src/components/Header/AboutUs.jsx
@@ -5,17 +5,17 @@ const AboutUs = () => {
   return (
     <section className="bg-dark xl:pt-10">
       <div className="relative pb-9 xl:pb-16 h-full items-center justify-center rounded-b-[5%] bg-dark2">
-        <div className="max-w-[90%] text-center h-[400px] flex justify-center mx-auto px-1 xl:pt-6 items-center  pt-28">
+        <div className="max-w-[90%] text-center h-[200px] md:h-[400px] flex justify-center mx-auto px-1 xl:pt-6 items-center  pt-24">
           {/* Background Image */}
           <img
             src={BackgroundImg}
             alt="Underline Effect"
-            className="absolute w-auto h-auto top-[66%] md:top-[70%] xl:top-[60%] 
+            className="absolute  h-auto top-[78%] md:top-[70%] xl:top-[60%] 
               left-1/2 
               transform 
               -translate-x-1/2 
               -translate-y-[70%]  /* Adjust this value to align with the text */
-              sm:w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 
+              w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] 
               opacity-80" /* Adjust image opacity */
           />
 
